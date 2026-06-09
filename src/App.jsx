31a6c4a2c53cefda18db2aee1e2e1785751a857d
davidmnhat <vominhnhat181005@gmail.com>
2026-06-09 15:18:02 +0700

    add a search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
 import './App.css'
 import { students } from './data/mock'
-
+import { useState } from 'react'
 function App() {
   console.log(students)
+  
+  const [searchTerm, setSearchTerm] = useState('') //lưu từ khóa tìm kiếm
+  const [searchQuery, setSearchQuery] = useState('') //lưu kết quả tìm kiếm
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchQuery(searchTerm);
+  }
+  const filteredStudents = students.filter(student =>
+    student.name.toLowerCase().includes(searchQuery.toLowerCase())
+  )
 
   return (
     <div>
       <h1>Intern Practice</h1>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Tìm theo tên..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
       <ul>
-        {students.map(student => (
-          <li key = {student.id}> {student.name} - {student.class} - {student.hobbies}</li>
-        ))}
+        {filteredStudents.length > 0 ?(
+          filteredStudents.map(student => (
+            <li key = {student.id}> {student.name} - {student.class} - {student.hobbies}</li>
+          ))
+        ) : (
+          <p>Không tìm thấy</p>
+        )}
       </ul>
     </div>
   )
